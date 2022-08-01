9136440c74cb3a840a288f5cceaf045a106cc16d
Benazir sultana <benazirsultana1998@gmail.com>
2022-08-01 19:00:00 +0530

    navbarcomplete

---
+++ Components/header.js
@@ -0,0 +1,37 @@
+function header(){
+    return `<nav>
+    <div id="one">
+        <h5><a href="">India Today</a></h5>
+        <h5><a href="">Malayalam</a></h5>
+        <h5><a href="">IndiaTodayNE</a></h5>
+        <h5><a href="">Business Today</a></h5>
+        <h5><a href="">DailyO</a></h5>
+        <h5><a href="">Aaj Tak</a></h5>
+        <h5><a href="">Lallantop</a></h5>
+        <h5><a href="">Bangla</a></h5>
+        <h5><a href="">GNTTV</a></h5>
+        <h5><a href="">iChowk</a></h5>
+        <h5><a href="">Reader's Digest</a></h5>
+    </div>
+    <div id="two">
+    <button id="sidebar1"><img src="../Styles/icons/icons8-menusidebar.svg" alt=""></button>
+        <h3><a href="">NEWS</a></h3>
+        <h3 >
+        <img src="https://akm-img-a-in.tosshub.com/sites/all/themes/itg/images/livedot.gif" alt="">
+        <a href="">
+        
+        LIVE TV</a></h3>
+        <div><a href="" id="logo"><img
+                    src="https://akm-img-a-in.tosshub.com/indiatoday/../sites/all/themes/itg/logo.png?v=1.52"
+      
+                    alt="Home" class="header__logo-image" pinger-seen="true"></a></div>
+    
+    
+    
+                    <h3><a href="">APP</a></h3>
+                    <h3><a href="">MAGAZINE</a></h3>
+                </div>
+
+</nav>`
+}
+export {header}
---
+++ index.js
@@ -0,0 +1,14 @@
+import {header} from "./Components/header.js"
+import {navbar} from "./Components/navbar.js"
+document.getElementById("navbar").innerHTML=navbar()
+document.getElementById("searchbar").style.display="none"
+document.getElementById("head").innerHTML=header()
+document.getElementById("searchicon").addEventListener("click",function(){
+    displaysearchbar()
+})
+function displaysearchbar(){
+   
+    var searchbar = document.getElementById("searchbar");
+  if(searchbar.style.display === '') searchbar.style.display = 'block'
+  else searchbar.style.display = searchbar.style.display === 'none' ? 'block' : 'none' 
+}
