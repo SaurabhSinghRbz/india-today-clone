310d3ad67402f2c90261422cbb8e4332d208ee95
Saurabh Singh <singhsaurabh162000@gmail.com>
2022-08-02 18:46:11 +0530

    js for expand footer

---
+++ Scripts/detailedArticle.js
@@ -0,0 +1,12 @@
+let hiderFalg = true;
+let hiderDiv = document.getElementById("hider").addEventListener('click', function () {
+    if (hiderFalg) {
+        document.getElementById("footerBlock2").style.display = "none"
+        document.getElementById("hiderImg").src = "../Images/footer/expand.png"
+        hiderFalg = false;
+    } else {
+        document.getElementById("footerBlock2").style.display = "grid"
+        document.getElementById("hiderImg").src = "../Images/footer/hide.png"
+        hiderFalg = true
+    }
+})
