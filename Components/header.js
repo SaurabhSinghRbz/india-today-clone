0568d00d3b1eda9d0fde5f8180c0530923bb16c7
Saurabh Singh <singhsaurabh162000@gmail.com>
2022-08-10 23:08:28 +0530

    updating header utillity

--- Components/header.js
+++ Components/header.js
@@ -1,7 +1,7 @@
 function header() {
   return `<nav>
     <div id="one">
-        <h5><a href="">India Today</a></h5>
+        <h5><a href="../index.html">India Today</a></h5>
         <h5><a href="">Malayalam</a></h5>
         <h5><a href="">IndiaTodayNE</a></h5>
         <h5><a href="">Business Today</a></h5>
@@ -16,12 +16,11 @@ function header() {
     <div id="two">
     <button id="sidebar1"><img style="height:20px;"src="../Images/icons/icons8-menusidebar.svg" alt=""></button>
         <h3><a href="">NEWS</a></h3>
-        <h3 >
+        <h3>
         <img src="https://akm-img-a-in.tosshub.com/sites/all/themes/itg/images/livedot.gif" alt="">
-        <a href="">
-        
-        LIVE TV</a></h3>
-        <div><a href="" id="logo"><img
+        <a href="../Pages/liveTv.html">LIVE TV</a>
+        </h3>
+        <div><a href="../index.html" id="logo"><img
                     src="https://akm-img-a-in.tosshub.com/indiatoday/../sites/all/themes/itg/logo.png?v=1.52"
       
                     alt="Home" class="header__logo-image" pinger-seen="true"></a></div>
@@ -31,8 +30,7 @@ function header() {
                     <h3><a href="">APP</a></h3>
                     <h3><a href="">MAGAZINE</a></h3>
                     <hr style="height:35px;">
-                    <span id="mobilebitcoin"
-                     class="desk-hide bitcoin-micon"><a href="https://www.indiatoday.in/interactive/cryptocurrency-price-in-india-bitcoin-ethereum-dogecoin-ripple-litecoin/" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" width="54.799" height="40.255" viewBox="0 0 54.799 40.255"> <g id="Bitcoins-logo-Dark_mode" data-name="Bitcoins-logo-Dark mode" transform="translate(0 0)"> <path id="Path_3367" data-name="Path 3367" d="M123.012,73.331c1.081.284,1.3,1.06,1.311,2.071.016,1.537.157,3.073.23,4.611a1.065,1.065,0,0,1-1.077,1.258c-.936.071-1.363-.222-1.456-1.049-.065-.579-.08-1.163-.131-1.941-.3.384-.484.622-.667.861q-3.833,5-7.666,10.006a1.456,1.456,0,0,1-2.239.459c-1.756-.938-3.52-1.859-5.265-2.817a.609.609,0,0,0-.853.127q-4.537,4.247-9.105,8.46a11.46,11.46,0,0,1-.932.706H94.57c-.2-.18-.417-.347-.6-.543a1.147,1.147,0,0,1,.052-1.764q4.249-3.953,8.5-7.9.89-.826,1.781-1.651a1.41,1.41,0,0,1,1.927-.256q2.664,1.414,5.326,2.834c.119.063.244.113.408.189l7.406-9.671-.076-.128-1.821.581c-.939.3-1.473.05-1.742-.822-.264-.855.018-1.335.939-1.63q2.842-.912,5.679-1.837c.106-.034.184-.075.329-.113A.663.663,0,0,1,123.012,73.331Z" transform="translate(-69.757 -64.576)" fill="#1f6feb"></path> <path id="Path_3366" data-name="Path 3366" d="M6.421,34.481V29.194H0l.635-3.811c.791,0,1.564,0,2.337,0a1.1,1.1,0,0,0,1.161-1.129c.009-.287,0-.575,0-.862,0-4.333-.012-8.667.009-13A1.639,1.639,0,0,0,2.423,8.626c-.733-.022-1.468,0-2.2,0h-.2V5.235h6.4V0H9.581V5.084H12.12V0h3.209c0,.319-.013.637-.013.956q0,1.975,0,3.95v.3c.525.069,1.037.122,1.543.2a9.9,9.9,0,0,1,3.884,1.336A4.838,4.838,0,0,1,23,10.42a6.273,6.273,0,0,1-.241,2.872,4.72,4.72,0,0,1-2.246,2.557c-.052.029-.1.061-.116.069.63.26,1.293.477,1.9.8a4.735,4.735,0,0,1,2.57,4.076,9.185,9.185,0,0,1-.381,3.6,5.741,5.741,0,0,1-2.929,3.445,10.25,10.25,0,0,1-3.275,1.035c-.973.147-1.952.252-2.949.379a.894.894,0,0,0-.015.131q0,2.478,0,4.956c0,.047.009.095.013.142H12.12v-5.2H9.581v5.2ZM9.687,25.4c.05.007.081.014.111.014a28.532,28.532,0,0,0,4.554-.183,7.45,7.45,0,0,0,2.812-.894,2.824,2.824,0,0,0,1.13-3.723A3.141,3.141,0,0,0,16.5,19.065a10.623,10.623,0,0,0-3.773-.67c-.924-.03-1.85,0-2.775,0-.086,0-.172.01-.269.017Zm0-10.21a3.108,3.108,0,0,0,.334.023c1.114-.041,2.23-.054,3.341-.141a5.892,5.892,0,0,0,2.334-.688,2.724,2.724,0,0,0,.938-3.943A3.409,3.409,0,0,0,14.682,9.2a10.173,10.173,0,0,0-2.756-.36c-.745-.016-1.491,0-2.245,0Z" transform="translate(10.081 0) rotate(17)" fill="#575757"></path> </g></svg></a></span>
+                    
                 </div>
 
 </nav>`;
